4ff3521a289e434c93ed1acf19d48c0cce164697
Roman Pavlenko <m9nnmsnrhc@privaterelay.appleid.com>
2026-01-15 19:27:13 +0100

    Delete sql/sql directory

--- sql/sql/03_create_clean_table.sql
+++
@@ -1,9 +0,0 @@
--- Create clean analytical table
-CREATE TABLE hotel_bookings_clean AS
-SELECT
-  hotel,
-  is_canceled::INT,
-  adr::NUMERIC,
-  is_repeated_guest::INT
-FROM hotel_bookings_raw
-WHERE adr IS NOT NULL AND adr::NUMERIC >= 0;
